c529941b2f0d6f538b2de5684216be0a297a4cea
Hermand PESSEK <pessek@bitbucket.org>
2021-05-31 12:11:51 +0000

    First Release

---
+++ elgg-plugin.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+	'bootstrap' => \hypeJunction\Illustration\Bootstrap::class,
+	'actions' => [
+		'illustrations/embed' => [
+			'controller' => \hypeJunction\Illustration\GenerateEmbedCode::class,
+		],
+		'cover/pick' => [
+			'controller' => \hypeJunction\Illustration\CoverPickAction::class,
+		],
+	],
+	'routes' => [
+		'illustrations:callback' => [
+			'path' => '/illustrations/callback',
+			'controller' => \hypeJunction\Illustration\UnsplashCallbackController::class,
+		],
+		'illustrations:search' => [
+			'path' => '/illustrations/search',
+			'controller' => \hypeJunction\Illustration\IllustrationSearchController::class,
+			'middleware' => [
+				\Elgg\Router\Middleware\AjaxGatekeeper::class,
+			],
+		],
+		'cover:pick' => [
+			'path' => '/cover/pick/{guid}',
+			'resource' => 'hero/cover/pick',
+		],
+	],
+];
